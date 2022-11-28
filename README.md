ae5fd70bd43b6fa39d9983bf109629f1a77818c0
ssluciwrt <tang828875@gmail.com>
2022-11-28 17:32:12 +0800

    commit source/Makefile/gitignore

---
+++ README.md
@@ -0,0 +1,10 @@
+# A test repository
+
+* Content
+
+  a simple helloworld program
+
+* Objective
+
+  1. create a local repo
+  2. push it to remote
---
+++ hello.c
@@ -0,0 +1,11 @@
+// hello.c -- a simple greeting program
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    if(argc == 1)
+	printf("\nHello world!\n\n");
+    else if(argc >= 2)
+	printf("\nHello %s!\n\n", argv[1]);
+    return 0;
+} 
